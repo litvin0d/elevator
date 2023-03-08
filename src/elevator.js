6b865bf76a9553401bfb2ca424aa46048a0df2d8
litvin0d <litvinovd944@gmail.com>
2023-03-08 16:08:00 +0500

    rewritten with OOP approach

--- src/elevator.js
+++ src/elevator.js
@@ -1,57 +1,111 @@
 class Elevator {
-    constructor(timeout) {
-        this.elevator = document.querySelector("#elevator");
-        this.currentFloor = 1;
-        this.firstFloorY = 0;
-        this.secondFloorY = -104;
-        this.thirdFloorY = -208;
-        this.fourthFloorY = -312;
-        this.activeCalls = [];
-        this.destinationFloors = [];
-        this.timeout = timeout;
-    }
+    #elevator = document.querySelector("#elevator"); // лифт
+    #firstFloorY = 0; // координаты 1 этажа
+    #secondFloorY = -104; // координаты 2 этажа
+    #thirdFloorY = -208; // координаты 3 этажа
+    #fourthFloorY = -312; // координаты 4 этажа
+    #activeCalls = []; // массив активных кнопок вызова
+    #destinationFloors = []; // массив этажей, на которых был вызван лифт
+    #timeout = 3000; // время передвижения и нахождения лифта на этаже
 
-    // добавление активным кнопкам класса call-active
-    setActiveCall() {
-        this.activeCalls.forEach(activeCall => {
+    // добавляет активным кнопкам класс .call-active (подсвечивание кнопки)
+    #setActiveCall() {
+        this.#activeCalls.forEach(activeCall => {
+            // если у кнопки уже есть класс, он не будет добавлятья повторно
             !activeCall.classList.contains("call-active") && activeCall.classList.add("call-active");
         });
     }
 
-    removeDestination() {
-        if (this.destinationFloors.length !== 0 && this.activeCalls !== 0) {
-            const activeCall = this.activeCalls.shift();
-            const destinationFloor = this.destinationFloors.shift();
-            if (+activeCall?.innerText === destinationFloor) activeCall?.classList.remove("call-active");
-            console.log("removed:", destinationFloor);
+    // удаляет кнопку и этаж, где была нажата кнопка из массивов
+    #removeDestination(index) {
+        // при условии, что массивы не пусты
+        if (this.#destinationFloors.length !== 0 && this.#activeCalls.length !== 0) {
+            // при условии, что номер кнопки и этаж совпадают
+            if (+this.#activeCalls[index].innerText === this.#destinationFloors[index]) {
+                this.#activeCalls[index].classList.remove("call-active"); // удаляет класс .call-active у кнопки
+                this.#activeCalls.splice(index, 1);  // удаляет кнопку из массива
+                this.#destinationFloors.splice(index, 1); // удаляет этаж из массива
+            }
         }
+    }
+
+    // перемещает лифт на нужный этаж
+    #goTo(floorY, index) {
+        setTimeout(() => {
+            this.#elevator.style.opacity = 0.5; // затемняет лифт при движении
+            this.#elevator.style.transform = `translateY(${floorY}px)`;
+            setTimeout(() => {
+                this.#elevator.style.opacity = 1;
+                this.#removeDestination(index); // после достижения этажа, убирает его с массивов
+            }, this.#timeout);
+        }, this.#timeout * index);
+    }
+
+    // возвращает лифт на нужный этаж
+    #comeBack() {
+        setTimeout(() => {
+            this.#elevator.style.opacity = 0.5;
+            setTimeout(() => {
+                this.#elevator.style.opacity = 1;
+            }, this.#timeout);
+            this.#elevator.style.transform = `translateY(${this.#firstFloorY}px)`;
+        }, this.#timeout * 3);
     }
 
     // заполнение массива активных кнопок и массива этажей назначения
     getDestinationFloors(activeCall) {
-        !this.activeCalls.includes(activeCall) && this.activeCalls.push(activeCall);
-        !this.destinationFloors.includes(+activeCall.innerText) && this.destinationFloors.push(+activeCall.innerText);
-        console.log(this.destinationFloors);
-        this.setActiveCall();
+        // при условии, что кнопки ещё нет в списке
+        !this.#activeCalls.includes(activeCall) && this.#activeCalls.push(activeCall);
+        // при условии, что этажа ещё нет в списке
+        !this.#destinationFloors.includes(+activeCall.innerText) && this.#destinationFloors.push(+activeCall.innerText);
+        this.#setActiveCall(); // подсвечивает кнопку
     }
 
-    move() {
-        const interval = setInterval(() => {
-            this.removeDestination();
-        }, this.timeout);
-        if (this.destinationFloors.length === 0 && this.activeCalls === 0) {
-            clearInterval(interval);
-        }
+    // обрабатывает массив назначеных этажй
+    handler() {
+        this.#destinationFloors.forEach((destinationFloor, index, destinationFloors) => {
+            if (destinationFloor === 2) {
+                this.#goTo(this.#secondFloorY, index);
+                this.#comeBack();
+            } else if (destinationFloor === 3) {
+                this.#goTo(this.#thirdFloorY, index);
+                if (destinationFloors[index + 1] === 2) { // при условии, что после 3 этажа следует 2
+                    this.#goTo(this.#secondFloorY, index + 1);
+                    this.#comeBack();
+                } else {
+                    this.#comeBack();
+                }
+            } else if (destinationFloor === 4) {
+                this.#goTo(this.#fourthFloorY, index);
+
+                if (destinationFloors[index + 1] === 3) { // при условии, что после 4 этажа следует 3
+                    this.#goTo(this.#thirdFloorY, index + 1);
+                    this.#comeBack();
+
+                    if (destinationFloors[index + 2] === 2) { // при условии, что после 4 этажа следует 3, а после 3 следует 2
+                        this.#goTo(this.#secondFloorY, index + 2);
+                        this.#comeBack();
+                    } else {
+                        this.#comeBack();
+                    }
+                } else if (destinationFloors[index + 1] === 2) { // при условии, что после 4 этажа следует 2
+                    this.#goTo(this.#secondFloorY, index + 1);
+                    this.#comeBack();
+                } else {
+                    this.#comeBack();
+                }
+            }
+        });
     }
 }
 
-const elevator = new Elevator(3000);
+const elevator = new Elevator();
 
+// навешивает на каждую кнопку обработчик событий с методами из класса
 const buttons = document.querySelectorAll(".call");
 buttons.forEach(button => {
     button.addEventListener("click", () => {
-        // button.classList.add("call-active");
         elevator.getDestinationFloors(button);
-        elevator.move();
+        elevator.handler();
     });
 });
